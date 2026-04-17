a1c51d2b953731b9e0f88d53ea786fe82f5a9c30
fav2308 <fav2308@gmail.com>
2026-04-17 12:03:09 -0300

    Update entrypoint.sh

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -6,6 +6,12 @@ if [ -f /var/run/nginx.pid ]; then
     rm /var/run/nginx.pid
 fi
 
+# Cria o banco SQLite se não existir
+touch /var/www/database/database.sqlite
+
+# Roda as migrations
+php /var/www/artisan migrate --force
+
 # Inicia o PHP-FPM em background
 php-fpm &
 
